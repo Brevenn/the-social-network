5ba622a5d05c04b3501a5ba11e61a272a628599f
Brevenn <Brevenjh@gmail.com>
2022-11-07 21:25:53 -0600

    added user and word routes to sub route index

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const wordRoutes = require("./word-routes");
+
+router.use("/users", userRoutes);
+router.use("/words", wordRoutes);
+
+module.exports = router;
