4563c74ded9e1f505c2cb535e587abe3cda3fbcd
Brevenn <Brevenjh@gmail.com>
2022-11-09 16:31:46 -0600

    added routes for the user models

---
+++ routes/api/user-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require("../../controllers/user-controller");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/").get(getSingleUser).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(uodateUser).delete(deleteUser);
+
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
